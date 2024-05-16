c1b9a4bfe2bbef66450e6dd496c65d00e9b2612b
Dominik <d.jasinski@ideo.pl>
2024-05-16 10:31:23 +0200

    feat: updates and fixes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,10 +1,66 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReactConfig,
-];
+module.exports = {
+   root: true,
+   parser: '@babel/eslint-parser',
+   settings: {
+      react: {
+         version: 'detect',
+      },
+   },
+   env: {
+      node: true,
+      browser: true,
+      es6: true,
+      commonjs: true,
+   },
+   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+   rules: {
+      'import/prefer-default-export': 0,
+      'no-console': 'warn',
+      'no-nested-ternary': 0,
+      'no-underscore-dangle': 0,
+      'no-unused-expressions': [
+         'error',
+         {
+            allowTernary: true,
+         },
+      ],
+      camelcase: 0,
+      'react/self-closing-comp': 1,
+      'react/jsx-filename-extension': [
+         1,
+         {
+            extensions: ['.js', 'jsx'],
+         },
+      ],
+      'react/prop-types': 0,
+      'react/destructuring-assignment': 0,
+      'react/jsx-no-comment-textnodes': 0,
+      'react/jsx-props-no-spreading': 0,
+      'react/no-array-index-key': 0,
+      'react/no-unescaped-entities': 0,
+      'react/require-default-props': 0,
+      'react/react-in-jsx-scope': 'off',
+      'linebreak-style': ['error', 'unix'],
+      semi: ['error', 'always'],
+      'prettier/prettier': [
+         'error',
+         {
+            endOfLine: 'auto',
+         },
+         {
+            usePrettierrc: true,
+         },
+      ],
+   },
+   parserOptions: {
+      ecmaFeatures: {
+         jsx: true,
+      },
+      ecmaVersion: 2020,
+      requireConfigFile: false,
+      babelOptions: {
+         presets: ['@babel/preset-react'],
+      },
+   },
+   plugins: ['react', '@next/eslint-plugin-next', 'prettier'],
+};
