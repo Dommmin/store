14360dfb9f9c470fca1cbf8cc08aff80613f4314
Dominik <d.jasinski@ideo.pl>
2024-05-20 20:17:27 +0200

    test

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -7,10 +7,6 @@ cd /root
 sudo docker compose -f docker-compose.prod.yml down
 sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
-sudo docker compose -f docker-compose.prod.yml exec -T api chmod -R 755 /usr/src/storage/logs
-sudo docker compose -f docker-compose.prod.yml exec -T api chown -R www-data:www-data /usr/src/storage/logs
-
-# Wykonanie komend cacheowania
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan config:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan route:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan view:cache
