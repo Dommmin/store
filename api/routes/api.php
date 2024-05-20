38a5930828c970d932b9bf4fa8a296348a8221f7
Dominik <d.jasinski@ideo.pl>
2024-05-20 10:40:40 +0200

    feat: update auth routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,12 +13,29 @@ use App\Http\Controllers\Api\v1\PaymentController;
 use App\Http\Controllers\Api\v1\ProductController;
 use App\Http\Controllers\Api\v1\ReviewController;
 use App\Http\Controllers\Api\v1\SearchController;
+use App\Http\Controllers\Auth\CurrentUserController;
 use App\Http\Controllers\Auth\SocialiteLoginController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\TwoFactorAuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
 Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:sanctum');
+
+Route::delete('/user', [CurrentUserController::class, 'destroy'])
+    ->middleware('auth:sanctum')
+    ->name('user.destroy');
+
+Route::get('/two-factor-authentication-enabled', [
+    TwoFactorAuthenticationController::class, 'enabled',
+])->name('two-factor-authentication.enabled');
+
+Route::get('/two-factor-authentication-challenge', [
+    TwoFactorAuthenticationController::class, 'challenge',
+])->name('two-factor-authentication.challenge')
+    ->middleware(['guest:' . config('fortify.guard')]);
 
 Route::prefix('auth/{provider}')->group(function (): void {
     Route::get('/url', [SocialiteLoginController::class, 'redirectToProvider']);
