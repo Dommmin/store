4b4756d5306ee1771414875fc3bca72619bf282c
Dominik <d.jasinski@ideo.pl>
2024-05-20 12:57:40 +0200

    feat: updates

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
       'no-nested-ternary': 0,
       'no-underscore-dangle': 0,
       'no-unused-expressions': 0,
-      camelcase: 0,
+      'camelcase': 0,
       'react/self-closing-comp': 1,
       'react/jsx-filename-extension': [
          1,
