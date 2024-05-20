ef1b6a49c6974c3c5d56808c7574e7cf85876a4c
Dominik <d.jasinski@ideo.pl>
2024-05-20 22:28:53 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,15 @@ set -e
 
 cd /root
 
+cp /api/.env.example /api/.env
+
 sudo docker compose -f docker-compose.prod.yml down
 sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan config:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan route:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan view:cache
+
+cd api && cp .env.prod .env
+
+sudo docker compose -f docker-compose.prod.yml exec -T api bash migration.sh
