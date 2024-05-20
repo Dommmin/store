cd00f4e54b3509d619d35432758d0d1e66a679cf
Dominik <d.jasinski@ideo.pl>
2024-05-20 18:59:59 +0200

    feat: add permissions

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-# Zmień katalog na główny katalog projektu
 cd /root
 
 sudo docker compose -f docker-compose.prod.yml down
 sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
+sudo docker compose -f docker-compose.prod.yml exec -T api chmod -R 755 /usr/src/storage/logs
+sudo docker compose -f docker-compose.prod.yml exec -T api chown -R www-data:www-data /usr/src/storage/logs
+
+# Wykonanie komend cacheowania
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan config:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan route:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan view:cache
