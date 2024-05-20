1f8d223f4738c93f3d4bb5cadd724220078cf758
Dominik <d.jasinski@ideo.pl>
2024-05-20 13:13:09 +0200

    fix: docker compose

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -4,9 +4,9 @@ set -e
 
 cd /usr/src
 
-sudo docker-compose -f docker-compose.prod.yml down
-sudo docker-compose -f docker-compose.prod.yml up -d --remove-orphans
+sudo docker compose -f docker-compose.prod.yml down
+sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan config:cache
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan route:cache
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan view:cache
+sudo docker compose -f docker-compose.prod.yml exec -T api php artisan config:cache
+sudo docker compose -f docker-compose.prod.yml exec -T api php artisan route:cache
+sudo docker compose -f docker-compose.prod.yml exec -T api php artisan view:cache
