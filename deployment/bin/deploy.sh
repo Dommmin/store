bb72ef8c6219ed475327a957f76eb7c0081191c5
Dominik <d.jasinski@ideo.pl>
2024-05-20 20:23:25 +0200

    test

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -7,6 +7,8 @@ cd /root
 sudo docker compose -f docker-compose.prod.yml down
 sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
+sudo docker exec -it -u root "$DOCKER_PREFIX"_api chown -R www-data:www-data storage
+
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan config:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan route:cache
 sudo docker compose -f docker-compose.prod.yml exec -T api php artisan view:cache
