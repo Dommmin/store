3ba02003b6647f7e4ceceba206b75045cb235e79
Dominik <d.jasinski@ideo.pl>
2024-05-20 11:59:51 +0200

    feat: eslint and prettier updates

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,66 +1,35 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-   root: true,
-   parser: '@babel/eslint-parser',
-   settings: {
-      react: {
-         version: 'detect',
-      },
-   },
-   env: {
-      node: true,
-      browser: true,
-      es6: true,
-      commonjs: true,
-   },
-   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
-   rules: {
-      'import/prefer-default-export': 0,
-      'no-console': 'warn',
-      'no-nested-ternary': 0,
-      'no-underscore-dangle': 0,
-      'no-unused-expressions': [
-         'error',
-         {
-            allowTernary: true,
-         },
-      ],
-      camelcase: 0,
-      'react/self-closing-comp': 1,
-      'react/jsx-filename-extension': [
-         1,
-         {
-            extensions: ['.js', 'jsx'],
-         },
-      ],
-      'react/prop-types': 0,
-      'react/destructuring-assignment': 0,
-      'react/jsx-no-comment-textnodes': 0,
-      'react/jsx-props-no-spreading': 0,
-      'react/no-array-index-key': 0,
-      'react/no-unescaped-entities': 0,
-      'react/require-default-props': 0,
-      'react/react-in-jsx-scope': 'off',
-      'linebreak-style': ['error', 'unix'],
-      semi: ['error', 'always'],
-      'prettier/prettier': [
-         'error',
-         {
-            endOfLine: 'auto',
-         },
-         {
-            usePrettierrc: true,
-         },
-      ],
-   },
-   parserOptions: {
-      ecmaFeatures: {
-         jsx: true,
-      },
-      ecmaVersion: 2020,
-      requireConfigFile: false,
-      babelOptions: {
-         presets: ['@babel/preset-react'],
-      },
-   },
-   plugins: ['react', '@next/eslint-plugin-next', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    plugins: ['react'],
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-unused-vars': 1,
+        'no-undef': 1,
+        'import/prefer-default-export': 0,
+        'no-console': 'warn',
+        'no-nested-ternary': 0,
+        'no-underscore-dangle': 0,
+        'no-unused-expressions': 0,
+        camelcase: 0,
+        'react/self-closing-comp': 1,
+        'react/jsx-filename-extension': [
+            1,
+            {
+                extensions: ['.js', '.jsx'],
+            },
+        ],
+        'react/prop-types': 0,
+        'react/destructuring-assignment': 0,
+        'react/jsx-no-comment-textnodes': 0,
+        'react/jsx-props-no-spreading': 0,
+        'react/no-array-index-key': 0,
+        'react/no-unescaped-entities': 0,
+        'react/require-default-props': 0,
+        'react/react-in-jsx-scope': 0,
+        'linebreak-style': ['error', 'unix'],
+    },
 };
--- frontend/app/loading.js
+++ frontend/app/loading.js
@@ -1,5 +1,5 @@
 import LoadingSpinner from './ui/LoadingSpinner';
 
 export default function Loading() {
-    return <LoadingSpinner />
+    return <LoadingSpinner />;
 }
