436cec6b35d2ed006a460a8df805316d8b53a877
Dominik <d.jasinski@ideo.pl>
2024-05-20 09:12:24 +0200

    feat: add custom messages to fortify

--- api/app/Providers/FortifyServiceProvider.php
+++ api/app/Providers/FortifyServiceProvider.php
@@ -8,11 +8,16 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Contracts\RegisterResponse;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -22,6 +27,35 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request): JsonResponse|RedirectResponse
+            {
+                if ($request->wantsJson()) {
+                    $user = User::where('email', $request->email)->first();
+
+                    return response()->json([
+                        'message' => 'You have successfully logged in.',
+                        'token' => $user->createToken($request->email)->plainTextToken,
+                    ]);
+                }
+
+                return redirect()->intended(Fortify::redirects('login'));
+            }
+        });
+
+        $this->app->instance(RegisterResponse::class, new class implements LoginResponse {
+            public function toResponse($request): JsonResponse|RedirectResponse
+            {
+                $user = User::where('email', $request->email)->first();
+
+                return $request->wantsJson()
+                    ? response()->json([
+                        'message' => 'You have successfully registered.',
+                        'token' => $user->createToken($request->email)->plainTextToken,
+                    ], 200)
+                    : redirect()->intended(Fortify::redirects('register'));
+            }
+        });
     }
 
     /**
