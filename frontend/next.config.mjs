7bd2ac766a1607ae9ae7da9f6e1f38620c2b7fa9
Dominik <d.jasinski@ideo.pl>
2024-05-21 09:26:19 +0200

    fix: images.domains error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: {
-      domains: ['via.placeholder.com', 'www.gravatar.com', 'minio', 'localhost'],
-      remotePatterns: [
-         {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '9099',
-            pathname: '/**',
-         },
-      ],
-   },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'via.placeholder.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.gravatar.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'minio',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '9099',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
