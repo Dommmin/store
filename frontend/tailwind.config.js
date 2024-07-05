ac3f7906ad7aa3231953422da56f63e1387efb29
Dominik <d.jasinski@ideo.pl>
2024-07-05 10:26:10 +0200

    feat: update tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   darkMode: ['media', '[data-mode="dark"]'],
-   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './app/**/*.{js,jsx}', './src/**/*.{js,jsx}'],
-   prefix: '',
-   theme: {
-      container: {
-         center: true,
-         padding: '2rem',
-         screens: {
-            '2xl': '1400px',
-         },
-      },
-   },
-   plugins: [require('tailwindcss-animate'), require('daisyui')],
+    // darkMode: ['media', '[data-mode="dark"]'],
+    darkMode: ["media", "(prefers-color-scheme: dark)"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Poppins", "system-ui"],
+            },
+            boxShadow: {
+                card: "0px 0px 20px 0px rgba(0, 0, 0, 0.15)",
+            },
+            transitionDuration: {
+                DEFAULT: "300ms",
+            },
+            minHeight: {
+                24: "96px"
+            },
+            colors: {
+                cyan: {
+                    500: "#46b6bf",
+                    700: "#0f929a",
+                    200: "#bceff3"
+                },
+                red: {
+                    500: "#ee4322"
+                },
+                black: "#1d1e1c",
+                white: "#ffffff",
+                transparent: "transparent",
+            },
+        },
+        container: {
+            padding: "2rem",
+            center: true,
+            screens: {
+                '2xl': '1400px',
+            },
+        },
+    },
+    daisyui: {
+        themes: ["emerald", "dark"],
+    },
+    plugins: [
+        require('daisyui'),
+        require('tailwindcss-animate'),
+    ],
 };
