abbba658aa3f8adc96e4e0f69066a6fbf947eced
Dominik <d.jasinski@ideo.pl>
2024-07-08 14:23:56 +0200

    feat: add localhost:3000 to cors

--- api/config/cors.php
+++ api/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
