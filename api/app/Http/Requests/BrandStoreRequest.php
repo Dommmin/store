949a07a32f6521310b0aae0afad2eca18f4e9d55
Dominik <d.jasinski@ideo.pl>
2024-07-08 14:21:37 +0200

    feat: add unique name validation

--- api/app/Http/Requests/BrandStoreRequest.php
+++ api/app/Http/Requests/BrandStoreRequest.php
@@ -24,7 +24,7 @@ class BrandStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:brands,name'],
         ];
     }
 }
