eb5fe3e7ee1a60711b770b591c7626579d3599f0
Dominik <d.jasinski@ideo.pl>
2024-07-08 15:07:23 +0200

    feat: update telescope and reverb, install pulse

--- api/database/migrations/2024_07_08_123710_create_telescope_entries_table.php
+++ api/database/migrations/2024_07_08_123710_create_telescope_entries_table.php
@@ -1,12 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
+return new class extends Migration
 {
     /**
      * Get the migration connection name.
@@ -23,7 +21,7 @@ return new class() extends Migration
     {
         $schema = Schema::connection($this->getConnection());
 
-        $schema->create('telescope_entries', function (Blueprint $table): void {
+        $schema->create('telescope_entries', function (Blueprint $table) {
             $table->bigIncrements('sequence');
             $table->uuid('uuid');
             $table->uuid('batch_id');
@@ -40,7 +38,7 @@ return new class() extends Migration
             $table->index(['type', 'should_display_on_index']);
         });
 
-        $schema->create('telescope_entries_tags', function (Blueprint $table): void {
+        $schema->create('telescope_entries_tags', function (Blueprint $table) {
             $table->uuid('entry_uuid');
             $table->string('tag');
 
@@ -53,7 +51,7 @@ return new class() extends Migration
                 ->onDelete('cascade');
         });
 
-        $schema->create('telescope_monitoring', function (Blueprint $table): void {
+        $schema->create('telescope_monitoring', function (Blueprint $table) {
             $table->string('tag')->primary();
         });
     }
